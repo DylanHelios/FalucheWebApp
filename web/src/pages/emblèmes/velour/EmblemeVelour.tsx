e6c215019e392b290d332e9111bf676e8560d53f
Marchant Dylan <101345489+DylanHelios@users.noreply.github.com>
2026-02-04 22:10:07 +0100

    feat: :sparkles: Page for embleme Velour and translate

--- web/src/pages/emblèmes/velour/EmblemeVelour.tsx
+++ web/src/pages/emblèmes/velour/EmblemeVelour.tsx
@@ -1,18 +1,18 @@
 import { useTranslation } from "react-i18next"
 import { EmblemesList } from "../EmblemeList"
 
-export default function EmblemeSatin() {
+export default function EmblemeVelour() {
   const { t } = useTranslation()
 
   return (
     <div className="p-6">
       <div className="mb-8">
         <h1 className="text-3xl font-bold mb-2">
-          {t("sidebar.emblemes.satin")}
+          {t("sidebar.emblemes.velours")}
         </h1>
       </div>
 
-      <EmblemesList type="Satin" />
+      <EmblemesList type="Velour" />
     </div>
   )
 }
